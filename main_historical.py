2ee3b059fabfd20610f64a6b86af0ea537c81f9e
Jeroen van Lier <jeroencvlier@gmail.com>
2025-01-04 10:05:08 +0200

    Adjust verbosity level in parallel processing of historical_snapshot

--- main_historical.py
+++ main_historical.py
@@ -363,8 +363,8 @@ def historical_snapshot(kwargs):
 
 
 if __name__ == "__main__":
-    _ = Parallel(n_jobs=4, backend="threading", verbose=5)(
-        delayed(historical_snapshot)(kwargs) for kwargs in tqdm(inputs)
+    _ = Parallel(n_jobs=4, backend="threading", verbose=100)(
+        delayed(historical_snapshot)(kwargs) for kwargs in inputs
     )
 
     log.success("All Done")
