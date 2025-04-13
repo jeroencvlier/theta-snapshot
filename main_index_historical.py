45c84d1cd54a969ca70651e6028670a5aac73277
Jeroen van Lier <jeroencvlier@gmail.com>
2025-04-13 18:36:06 +0200

    reorder

--- main_index_historical.py
+++ main_index_historical.py
@@ -173,7 +173,7 @@ if __name__ == "__main__":
     # Input Parameters
     # --------------------------------------------------------------
     ivl = 900000  # 15 minutes
-    tickers = ["SPXW", "SPY", "XLE", "GLD", "DBO"]
+    tickers = ["SPY", "SPXW", "XLE", "GLD", "DBO"]
     max_trading_days = 45
 
     # --------------------------------------------------------------
